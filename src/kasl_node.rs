7128ca703b9c23d3be46c0e265f49049179eace9
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-03-23 15:17:14 +0900

    Update knodiq engine and adapt to the new node

--- src/kasl_node.rs
+++ src/kasl_node.rs
@@ -1,5 +1,5 @@
 use kasl::{KaslCompiler, scope_manager::IOBlueprint};
-use knodiq_engine::node::Node;
+use knodiq_engine::{audio_context::AudioContext, node::Node, type_registry::TypeInfo};
 use std::path::PathBuf;
 
 #[derive(Default)]
@@ -7,6 +7,10 @@ pub struct KaslNode {
     compiler: KaslCompiler,
     blueprint: Option<IOBlueprint>,
     search_paths: Vec<String>,
+    code: Option<String>,
+
+    input_types: Vec<TypeInfo>,
+    output_types: Vec<TypeInfo>,
 
     states: Vec<*mut ()>,
     is_first_process: bool,
@@ -21,13 +25,19 @@ impl KaslNode {
         self.search_paths = paths;
     }
 
-    pub fn compile(&mut self, code: &str) -> Result<(), Vec<kasl::error::ErrorRecord>> {
+    pub fn set_code(&mut self, code: String) {
+        self.code = Some(code);
+    }
+
+    pub fn compile(&mut self) -> Result<(), Vec<kasl::error::ErrorRecord>> {
         // Add the search paths to the compiler
         self.compiler
             .set_search_paths(self.search_paths.iter().map(PathBuf::from).collect());
 
         // Parse, build and compile the source codes
-        self.compiler.parse(code).map_err(|e| vec![*e])?;
+        self.compiler
+            .parse(self.code.as_ref().unwrap_or(&String::default()))
+            .map_err(|e| vec![*e])?;
         let blueprint = self.compiler.build()?;
 
         // Allocate the state memory based of the blueprint
@@ -46,21 +56,96 @@ impl KaslNode {
 
         // Set the blueprint
         self.blueprint = Some(blueprint);
+
+        // Update the types
+        self.update_type_infos();
+
         Ok(())
+    }
+
+    fn update_type_infos(&mut self) {
+        // Create TypeInfo for input types and output types
+        self.input_types = self
+            .blueprint
+            .as_ref()
+            .map(|blueprint| {
+                blueprint
+                    .get_inputs()
+                    .iter()
+                    .map(|item| TypeInfo::new(item.actual_size, item.align as usize))
+                    .collect()
+            })
+            .unwrap_or_default();
+        self.output_types = self
+            .blueprint
+            .as_ref()
+            .map(|blueprint| {
+                blueprint
+                    .get_outputs()
+                    .iter()
+                    .map(|item| TypeInfo::new(item.actual_size, item.align as usize))
+                    .collect()
+            })
+            .unwrap_or_default();
     }
 }
 
 impl Node for KaslNode {
-    fn prepare(&mut self, _audio_ctx: &knodiq_engine::audio_context::AudioContext) {
+    fn get_input_names(&self) -> Vec<String> {
+        self.blueprint
+            .as_ref()
+            .map(|blueprint| {
+                blueprint
+                    .get_inputs()
+                    .iter()
+                    .map(|i| i.name.clone())
+                    .collect()
+            })
+            .unwrap_or_default()
+    }
+
+    fn get_output_names(&self) -> Vec<String> {
+        self.blueprint
+            .as_ref()
+            .map(|blueprint| {
+                blueprint
+                    .get_outputs()
+                    .iter()
+                    .map(|i| i.name.clone())
+                    .collect()
+            })
+            .unwrap_or_default()
+    }
+
+    fn get_input_len(&self) -> usize {
+        self.blueprint
+            .as_ref()
+            .map(|blueprint| blueprint.get_inputs().len())
+            .unwrap_or_default()
+    }
+
+    fn get_output_len(&self) -> usize {
+        self.blueprint
+            .as_ref()
+            .map(|blueprint| blueprint.get_outputs().len())
+            .unwrap_or_default()
+    }
+
+    fn get_input_type(&self, index: usize) -> Option<&TypeInfo> {
+        self.input_types.get(index)
+    }
+
+    fn get_output_type(&self, index: usize) -> Option<&TypeInfo> {
+        self.output_types.get(index)
+    }
+
+    fn update(&mut self, _audio_ctx: &AudioContext) {}
+
+    fn prepare(&mut self, _audio_ctx: &AudioContext) {
         self.is_first_process = true;
     }
 
-    fn process(
-        &mut self,
-        inputs: &[*const u8],
-        outputs: &[*mut u8],
-        audio_ctx: &knodiq_engine::audio_context::AudioContext,
-    ) {
+    fn process(&mut self, inputs: &[*const u8], outputs: &[*mut u8], audio_ctx: &AudioContext) {
         let inputs: Vec<*const ()> = inputs.iter().map(|p| *p as *const ()).collect();
         let outputs: Vec<*mut ()> = outputs.iter().map(|p| *p as *mut ()).collect();
 
