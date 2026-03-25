4967a77c9e7d93ac737edfac2a801282a608c3e0
Hatya-mouse <158461506+Hatya-mouse@users.noreply.github.com>
2026-03-25 21:25:31 +0900

    Update Knodiq Engine and support as_any and as_any_mut

--- src/kasl_node.rs
+++ src/kasl_node.rs
@@ -169,6 +169,14 @@ impl Node for KaslNode {
 
         self.is_first_process = false;
     }
+
+    fn as_any(&self) -> &dyn std::any::Any {
+        self
+    }
+
+    fn as_any_mut(&mut self) -> &mut dyn std::any::Any {
+        self
+    }
 }
 
 impl Clone for KaslNode {
